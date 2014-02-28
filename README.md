9e18d3a3f92867b510ae69d2e8dabab72fd1246f
nmonti <nickmax123@gmail.com>
2014-02-28 18:40:21 -0500

    FUCK IT

--- README.md
+++
@@ -1,5 +0,0 @@
-Official GMLF Productions Website
-=======
-
-Deployed with heroku. Temporary site is
-[here](https://immense-bayou-1439.herokuapp.com). 
--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Gmlf::Application.config.secret_token = '93043d7a5a34f6aa82a7d112bffb18e6521488da0ed7a4c06611b541686b9a2ed323fad285b72224d1594d74869fa1b857a9303ab168333262f12be7d9cda755'
