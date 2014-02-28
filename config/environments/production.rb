707e5a976cd6b37edb8876fb89e39d3227a5cbc7
nmonti <nickmax123@gmail.com>
2014-02-27 23:31:35 -0500

    more bullshit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,6 +14,9 @@ Gmlf::Application.configure do
   # Compress JavaScripts and CSS
   config.assets.compress = true
 
+  #for fonts
+  config.assets.precompile += %w( .svg .eot .woff .ttf )
+
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
 
